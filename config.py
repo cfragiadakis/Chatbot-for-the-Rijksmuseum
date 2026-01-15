d5639efdd2691e161ce2782ec1db253d08566d92
Christoforos Fragkiadakis <cfragiadakis@hotmail.gr>
2026-01-15 14:54:58 +0100

    upload data extractor

---
+++ config.py
@@ -0,0 +1,4 @@
+# -----------------------------------------  DATA EXTRACTION ---------------------------------------------------------------
+search_set = {'Johannes Vermeer': ['The Milkmaid', 'The Love Letter', 'The Little Street'], 'Van Gogh': ['Self-Portrait']}
+folder_path = "Data"
+save_path = "Data/extracted_data.json"
